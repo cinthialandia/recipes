e2ecb31f9efb5477773f154c6cac5f89cfd24dee
Cinthia <cinthia.j.23@gmail.com>
2020-09-04 15:15:18 +1000

    using env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import "firebase/auth";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyBa_lp68yDYSJ6Cv9Q9ZwxE4S23-hVW8V4",
-  authDomain: "recipes-app-b5527.firebaseapp.com",
-  databaseURL: "https://recipes-app-b5527.firebaseio.com",
-  projectId: "recipes-app-b5527",
-  storageBucket: "recipes-app-b5527.appspot.com",
-  appId: "1:115217535029:web:e6c2c7fe918b01e843a17c",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = firebase.initializeApp(config);
