81d8051133eec38311f2f0841d1a476928fb8864
Cinthia <cinthia.j.23@gmail.com>
2020-09-10 19:29:09 +1000

    canges

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import Spinner from "react-bootstrap/Spinner";
 import { useAuth } from "./providers/AuthProvider";
 import AuthenticatedApp from "./AuthenticatedApp";
 import UnauthenticatedApp from "./UnauthenticatedApp";
+import "./App.scss";
 
 //copied from https://kentcdodds.com/blog/authentication-in-react-applications
 function App() {
   const { value: user, loading } = useAuth();
 
   return loading ? (
-    <Spinner className="loading" animation="grow" />
+    <div className="loading">
+      <Spinner animation="grow" />
+    </div>
   ) : user ? (
     <AuthenticatedApp />
   ) : (
